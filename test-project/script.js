525a9afe026801eb1b2f448fb98670b3bd7b3205
Luka <luka.baotian@gmail.com>
2017-11-13 21:53:29 +0100

    adding new function

--- test-project/script.js
+++ test-project/script.js
@@ -2,14 +2,24 @@
 //    console.log(i);
 //}
 
-var names = ['Luka','Bob','Darko'];
 
-for(var i = 0; i< names.length; i++){
-    console.log(names[i]);
+
+
+function printFullAge(years){
+var ages = [];
+
+for(var i = 0; i<years.length; i++){
+    ages[i] = 2017 - years[i];
+ }
+
+for(i = 0; i<ages.length; i++){
+    if(ages[i]>=18){
+     console.log(ages[i]);   
+    }
 }
-
-var broj = 23;
-
-if(broj>0){
-  console.log(broj);  
 }
+var years = [1992, 2000, 1990, 1950,1974];
+printFullAge(years);
+
+
+
